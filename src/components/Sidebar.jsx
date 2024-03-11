1f4dfdf06e65bd460d5316b5bc775e7781140284
aamir221 <aamirmanzoor372@gmail.com>
2024-03-11 11:00:40 +0530

    Side bar components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import { NavLink } from "react-router-dom";
 import { FaTasks } from "react-icons/fa";
 import { AiFillCalender, AiOutlineTable } from "react-icons/ai";
-import {MdSpaceDashboard } from "react-icons/md";
+import { MdSpaceDashboard } from "react-icons/md";
+import data from "../data/data";
 import css from "./Sidebar.module.css";
 
 const Sidebar = () => {
@@ -9,12 +10,20 @@ const Sidebar = () => {
     <div className={css.container}>
       <img src="./logo.png" alt="Logo" className={css.logo} />
       <div className={css.menu}>
-        <NavLink
-          to="dashboard"
-          className={css.icon}
-          title={"Dashboard"}
-        >
-            <MdSpaceDashboard size={30}/>
+        <NavLink to="dashboard" className={css.item} title={"Dashboard"}>
+          <MdSpaceDashboard size={30} />
+        </NavLink>
+
+        <NavLink to="calender" className={css.item} title={"Calender"}>
+          <AiFillCalender size={30} />
+        </NavLink>
+
+        <NavLink to="board" className={css.item} title={"Trello Board"}>
+          <FaTasks size={30} />
+        </NavLink>
+
+        <NavLink to="users" className={css.item} title={"Users"}>
+          <AiOutlineTable size={30} />
         </NavLink>
       </div>
     </div>
